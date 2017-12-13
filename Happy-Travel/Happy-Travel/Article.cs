39483c049db10c597b651bccb42845763203ee12
bellahuang <bellahuang1012@gmail.com>
2017-12-13 13:40:13 -0500

    create Article class

---
+++ Happy-Travel/Happy-Travel/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+
+namespace HappyTravel
+{
+    [Table("ArticleTable")]
+    public class Article
+    {
+        [PrimaryKey, AutoIncrement]
+        public int article_id { get; set; }
+
+        [NotNull]
+        public int user_id { get; set; }
+
+        [NotNull]
+        public string title { get; set; }
+
+        [NotNull]
+        public string content { get; set; }
+    }
+}
