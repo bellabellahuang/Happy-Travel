cfaceffed04bf7e837fd7e625aa1797f1ee99e79
bellahuang <bellahuang1012@gmail.com>
2017-12-14 14:30:20 -0500

    created ArticleListViewAdapter class

---
+++ Happy-Travel/Happy-Travel/ArticleListViewAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.Views;
+
+namespace HappyTravel
+{
+    public class ArticleListViewAdapter : BaseAdapter<Article>
+    {
+        private readonly Activity activity;
+        private List<Article> articleListData;
+        private UsersDB userDB = UsersDB.Users;
+
+        public ArticleListViewAdapter(Activity a, List<Article> articles) : base()
+        {
+            this.activity = a;
+            this.articleListData = articles;
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return articleListData.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override Article this[int position]
+        {
+            get
+            {
+                return articleListData[position];
+            }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            if(view == null){
+                view = activity.LayoutInflater.Inflate(Resource.Layout.ArticleItemLayout, null, false);
+            }
+            Article article = this[position];
+            view.FindViewById<TextView>(Resource.Id.txtItemTitle).Text = article.title;
+
+            userDB.CreateTable();
+            User author = userDB.GetUserById(article.user_id);
+
+            view.FindViewById<TextView>(Resource.Id.txtItemAuthor).Text = author.username;
+
+            return view;
+        }
+    }
+}
