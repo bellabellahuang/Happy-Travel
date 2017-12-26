1031c3976f62a9a0bf44a5dc23878b5dac9d6270
bellahuang <bellahuang1012@gmail.com>
2017-12-26 13:50:09 -0500

    show username in my profile

--- Happy-Travel/Happy-Travel/MyProfileActivity.cs
+++ Happy-Travel/Happy-Travel/MyProfileActivity.cs
@@ -16,10 +16,51 @@ namespace HappyTravel
     [Activity(Label = "MyProfileActivity")]
     public class MyProfileActivity : Activity
     {
+        private Button btnMe;
+        private Button btnHome;
+        private Button btnPost;
+
+        private TextView txtUsername;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.MyProfile);
+
+            Bundle bundleUser = Intent.Extras;
+            int userId = bundleUser.GetInt("userId");
+
+            var username = UsersDB.Users.GetUserById(userId).username;
+            txtUsername = FindViewById<TextView>(Resource.Id.txtMeUsername);
+            txtUsername.Text = username;
+
+            btnMe = FindViewById<Button>(Resource.Id.btnMe);
+            btnHome = FindViewById<Button>(Resource.Id.btnHome);
+            btnPost = FindViewById<Button>(Resource.Id.btnPost);
+
+            btnMe.Click += (object sender, EventArgs e) => {
+                Intent intentMe = new Intent(this, typeof(MyProfileActivity));
+                Bundle bundleMe = new Bundle();
+                bundleMe.PutInt("userId", userId);
+                intentMe.PutExtras(bundleMe);
+                StartActivity(intentMe);
+            };
+
+            btnHome.Click += (object sender, EventArgs e) => {
+                Intent intentHome = new Intent(this, typeof(ArticleListActivity));
+                Bundle bundleHome = new Bundle();
+                bundleHome.PutInt("userId", userId);
+                intentHome.PutExtras(bundleHome);
+                StartActivity(intentHome);
+            };
+
+            btnPost.Click += (object sender, EventArgs e) => {
+                Intent intentPost = new Intent(this, typeof(ArticlePublishingActivity));
+                Bundle bundlePost = new Bundle();
+                bundlePost.PutInt("userId", userId);
+                intentPost.PutExtras(bundlePost);
+                StartActivity(intentPost);
+            };
         }
     }
 }
