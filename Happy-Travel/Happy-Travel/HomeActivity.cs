309780e568bc9a45f6bde32b495c6b3e91cd7faa
bellahuang <bellahuang1012@gmail.com>
2017-12-25 21:59:59 -0500

    add menuButton click event in homeActivity

--- Happy-Travel/Happy-Travel/HomeActivity.cs
+++ Happy-Travel/Happy-Travel/HomeActivity.cs
@@ -16,45 +16,36 @@ namespace HappyTravel
     [Activity(Label = "Home-Travel")]
     public class HomeActivity : Activity 
     {
-        private LinearLayout tabMe;
-        private LinearLayout tabHome;
-        private LinearLayout tabPost;
-
-        private ImageView imgMe;
-        private ImageView imgHome;
-        private ImageView imgPost;
-
-        private TextView txtMe;
-        private TextView txtHome;
-        private TextView txtPost;
+        private Button btnMe;
+        private Button btnHome;
+        private Button btnPost;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Home);
 
+            btnMe = FindViewById<Button>(Resource.Id.btnMe);
+            btnHome = FindViewById<Button>(Resource.Id.btnHome);
+            btnPost = FindViewById<Button>(Resource.Id.btnPost);
 
-            initView();
+            btnMe.Click += (object sender, EventArgs e) => {
+                Intent intentMe = new Intent(this, typeof(MyProfileActivity));
+                StartActivity(intentMe);
+            };
 
-            tabPost.Click += (object sender, EventArgs e) => {
-                Intent showPost = new Intent(this.BaseContext, typeof(ArticlePublishingActivity));
-                StartActivity(showPost);
+            btnHome.Click += (object sender, EventArgs e) => {
+                Intent intentHome = new Intent(this, typeof(HomeActivity));
+                StartActivity(intentHome);
+            };
+
+            btnPost.Click += (object sender, EventArgs e) => {
+                Intent intentPost = new Intent(this, typeof(ArticlePublishingActivity));
+                StartActivity(intentPost);
             };
         }
 
-        private void initView(){
-            tabMe = FindViewById<LinearLayout>(Resource.Id.mTabMe);
-            tabHome = FindViewById<LinearLayout>(Resource.Id.mTabHome);
-            tabPost = FindViewById<LinearLayout>(Resource.Id.mTabPost);
 
-            imgMe = FindViewById<ImageView>(Resource.Id.imgMe);
-            imgHome = FindViewById<ImageView>(Resource.Id.imgHome);
-            imgPost = FindViewById<ImageView>(Resource.Id.imgPost);
-
-            txtMe = FindViewById<TextView>(Resource.Id.txtMe);
-            txtHome = FindViewById<TextView>(Resource.Id.txtHome);
-            txtPost = FindViewById<TextView>(Resource.Id.txtPost);
-        }
 
 
     }
