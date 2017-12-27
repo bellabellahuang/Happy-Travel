7f323d29dac1692203065096dce3925c68dff84b
bellahuang <bellahuang1012@gmail.com>
2017-12-27 10:29:55 -0500

    create comment class

---
+++ Happy-Travel/Happy-Travel/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+
+namespace HappyTravel
+{
+    [Table("CommentsTable")]
+    public class Comment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int comment_id { get; set; }
+
+        [NotNull]
+        public int article_id { get; set; }
+
+        [NotNull]
+        public int user_id { get; set; }
+
+        [NotNull]
+        public string comment { get; set; }
+    }
+}
