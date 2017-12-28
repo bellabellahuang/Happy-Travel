de5b0e8f9f421cbed3edf3bccd48d79601710170
bellahuang <bellahuang1012@gmail.com>
2017-12-28 14:04:30 -0500

    make code in ArticleDetailActivity more clearer

--- Happy-Travel/Happy-Travel/ArticleDetailActivity.cs
+++ Happy-Travel/Happy-Travel/ArticleDetailActivity.cs
@@ -21,36 +21,42 @@ namespace HappyTravel
         private TextView content;
         private ArticleDB articleDB = ArticleDB.Articles();
         private UsersDB userDB = UsersDB.Users;
-
         private Button btnMe;
         private Button btnHome;
         private Button btnPost;
+        int articleId;
+        int userId;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ArticleDetail);
 
-            title = FindViewById<TextView>(Resource.Id.txtDetailTitle);
-            author = FindViewById<TextView>(Resource.Id.txtDetailAuthor);
-            content = FindViewById<TextView>(Resource.Id.txtDetailContent);
+            // initiate views
+            initView();
 
-            var articleId = Intent.Extras.GetInt("Article");
-            var userId = Intent.Extras.GetInt("User");
+            // get article id and user id
+            articleId = Intent.Extras.GetInt("Article");
+            userId = Intent.Extras.GetInt("User");
 
-            articleDB.CreateTable();
-            userDB.CreateTable();
+            // get article instance and user instance
             var article = articleDB.GetArticleById(articleId);
             var Author = userDB.GetUserById(article.user_id);
 
+            // display the detail of article
             title.Text = article.title;
             author.Text = Author.username;
             content.Text = article.content;
 
-            btnMe = FindViewById<Button>(Resource.Id.btnMe);
-            btnHome = FindViewById<Button>(Resource.Id.btnHome);
-            btnPost = FindViewById<Button>(Resource.Id.btnPost);
+            // call the menuBar method to handle buttons click events
+            menuBar();
 
+
+        }
+
+        // menu buttons click event handler
+        private void menuBar()
+        {
             btnMe.Click += (object sender, EventArgs e) => {
                 Intent intentMe = new Intent(this, typeof(MyProfileActivity));
                 Bundle bundleMe = new Bundle();
@@ -74,6 +80,17 @@ namespace HappyTravel
                 intentPost.PutExtras(bundlePost);
                 StartActivity(intentPost);
             };
+        }
+
+        // assign values to widget variables
+        private void initView()
+        {
+            title = FindViewById<TextView>(Resource.Id.txtDetailTitle);
+            author = FindViewById<TextView>(Resource.Id.txtDetailAuthor);
+            content = FindViewById<TextView>(Resource.Id.txtDetailContent);
+            btnMe = FindViewById<Button>(Resource.Id.btnMe);
+            btnHome = FindViewById<Button>(Resource.Id.btnHome);
+            btnPost = FindViewById<Button>(Resource.Id.btnPost);
         }
     }
 }
